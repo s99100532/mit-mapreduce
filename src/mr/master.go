286cb79e4f9f4fd085e35e9a4c3dda68bf263c92
Ubuntu <ubuntu@ip-172-31-8-34.ap-southeast-1.compute.internal>
2022-05-05 07:25:40 +0000

    feat: add rpc for task request

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,15 +1,15 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
 type Master struct {
 	// Your definitions here.
-
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -23,7 +23,6 @@ func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -50,7 +49,6 @@ func (m *Master) Done() bool {
 
 	// Your code here.
 
-
 	return ret
 }
 
@@ -63,7 +61,6 @@ func MakeMaster(files []string, nReduce int) *Master {
 	m := Master{}
 
 	// Your code here.
-
 
 	m.server()
 	return &m
